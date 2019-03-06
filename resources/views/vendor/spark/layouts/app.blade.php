fad5e577b71d698b5d9f06f0a66ad93943044edb
Gilbert Pellegrom <gilbert@pellegrom.me>
2019-03-06 11:58:13 +0000

    Add favicon

--- resources/views/vendor/spark/layouts/app.blade.php
+++ resources/views/vendor/spark/layouts/app.blade.php
@@ -3,8 +3,10 @@
 <head>
     <!-- Meta Information -->
     <meta charset="utf-8">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" type="image/png" href="/favicon.png">
 
     <title>@yield('title', config('app.name'))</title>
 
