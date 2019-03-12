9149be50614802a577d624cfe2d6c372f69a0658
Gilbert Pellegrom <gilbert@pellegrom.me>
2019-03-12 15:41:12 +0000

    Show event output

--- resources/js/components/ScheduleMonitor/Events.vue
+++ resources/js/components/ScheduleMonitor/Events.vue
@@ -10,7 +10,8 @@
                         <tr>
                             <th>Sarted</th>
                             <th>Finished</th>
-                            <th class="text-right">Duration</th>
+                            <th>Duration</th>
+                            <th class="text-right">Output</th>
                         </tr>
                     </thead>
                     <tbody>
@@ -25,7 +26,13 @@
                                     <span class="text-muted ml-2">{{ fromNow(event.finished_at) }}</span>
                                 </span>
                             </td>
-                            <td class="text-right">{{ duration(event.duration) }}</td>
+                            <td>{{ duration(event.duration) }}</td>
+                            <td class="text-right">
+                                <a href="#" title="View Output" class="mr-3" @click.prevent="getOutput(event)" v-if="event.has_output">
+                                    <fa-icon icon="eye" />
+                                </a>
+                                <span class="text-muted mr-3" v-else>&mdash;</span>
+                            </td>
                         </tr>
                     </tbody>
                 </table>
@@ -34,8 +41,30 @@
                 <em>No pings yet...</em>
             </div>
         </div>
+
+        <portal to="default">
+            <modal
+                :show="showEventOutput"
+                title="Scheduled Job Output"
+                ok-text="Ok"
+                size="large"
+                @cancel="showEventOutput = false"
+                @ok="showEventOutput = false">
+                <pre class="event-output"><code>{{ output }}</code></pre>
+            </modal>
+        </portal>
     </div>
 </template>
+
+<style type="scss" scoped>
+.event-output {
+    white-space: pre-wrap;
+    min-height: 200px;
+    max-height: 800px;
+    overflow: auto;
+    margin-bottom: 0;
+}
+</style>
 
 <script>
 import datetime from '../../mixins/datetime';
@@ -48,6 +77,8 @@ export default {
     data() {
         return {
             events: [],
+            showEventOutput: false,
+            output: '',
         }
     },
 
@@ -67,6 +98,15 @@ export default {
                     this.events = response.data;
                 });
         },
+        getOutput(event) {
+            this.showEventOutput = true;
+            this.output = 'Loading...';
+
+            axios.get(`/api/schedule-monitor-events/${event.id}`)
+                .then(response => {
+                    this.output = response.data.data.output;
+                });
+        }
     }
 }
 </script>
