6d4ae79133881794f9caa74f47be23fe9fcfb700
Gilbert Pellegrom <gilbert@pellegrom.me>
2019-04-03 14:00:21 +0100

    Update EmailAlert.php

--- app/Alert/EmailAlert.php
+++ app/Alert/EmailAlert.php
@@ -2,22 +2,10 @@
 
 namespace App\Alert;
 
-use Illuminate\Database\Eloquent\Model;
-
-class EmailAlert extends Model
+class EmailAlert extends AlertModel
 {
     protected $fillable = [
         'team_id',
         'email',
     ];
-
-    public function team()
-    {
-        return $this->belongsTo('App\Team');
-    }
-
-    public function apps()
-    {
-        return $this->belongsToMany('App\App');
-    }
 }
