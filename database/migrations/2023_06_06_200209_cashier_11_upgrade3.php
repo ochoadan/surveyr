b11e63c74e06c7b1fdd57ae357ff4e82639ccbdf
Gilbert Pellegrom <gilbert@pellegrom.me>
2023-06-06 22:42:16 +0100

    Create 2023_06_06_200209_cashier_11_upgrade3.php

---
+++ database/migrations/2023_06_06_200209_cashier_11_upgrade3.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Cashier11Upgrade3 extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('team_subscriptions', function (Blueprint $table) {
+            $table->string('stripe_plan')->nullable()->change();
+            $table->integer('quantity')->nullable()->change();
+        });
+
+        Schema::create('team_subscription_items', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('subscription_id');
+            $table->string('stripe_id')->index();
+            $table->string('stripe_plan');
+            $table->integer('quantity');
+            $table->timestamps();
+
+            $table->unique(['subscription_id', 'stripe_plan']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
